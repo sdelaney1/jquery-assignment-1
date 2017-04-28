ce44fa10ec51319cd8389695a3ae2101665a3457
Shannon Delaney <sdelaney1@id.iit.edu>
2017-04-28 00:43:56 -0500

    no message

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,23 @@
 // Do your work in this file.
+
+
+$("<div>").addClass("view").appendTo(document.body);
+
+$("<div>").addClass("grass").appendTo(".view");
+
+$("<div>").addClass("sun").appendTo(".view");
+
+var i = 1;
+while (i <= 5) {
+    $("<div>").addClass("ray-" + i).appendTo(".sun");
+    i++;
+}
+
+$("<div>").addClass("mountain").appendTo(".view");
+$("<div>").addClass("mountain-top").appendTo(".mountain");
+
+var i = 1;
+do {
+    $("<div>").addClass("mountain-cap-" + i).appendTo(".mountain-top");
+    i++;
+} while (i <= 3)
--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,22 @@
 // Do your work in this file.
+
+for (var i = 1; i <= 196; i++) {
+    var remainder = i % 2;
+    switch(remainder) {
+        case 0:
+            $("<div>").addClass("box type-1").appendTo(document.body);
+            break;
+        case 1:
+            $("<div>").addClass("box type-2").appendTo(document.body);
+            break;
+    }
+}
+
+
+$(".box").click(function() {
+    if($(this).hasClass("clicked")) {
+        $(this).removeClass("clicked");
+    } else {
+        $(this).addClass("clicked");
+    }
+})
